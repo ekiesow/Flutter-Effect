a5b06717b0ef876a370ca1193746109510071127
Eric Kiesow <ekiesow@mail.csuchico.edu>
2019-05-04 22:37:48 -0700

    Manual merge of auth and latest build of UI in shared_pref.

--- flutter_effect/lib/main.dart
+++ flutter_effect/lib/main.dart
@@ -1,29 +1,31 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_effect/Home/home_screen.dart';
+
+import 'package:flutter_effect/Auth/auth.dart';
+import 'package:flutter_effect/Auth/auth_provider.dart';
+import 'package:flutter_effect/Auth/root_page.dart';
 import 'package:flutter_effect/Home/modules/scaffold/appBar_module.dart';
 import 'package:flutter_effect/Home/modules/scaffold/scaffold_module.dart';
 
 void main() => runApp(FlutterEffect());
 
 class FlutterEffect extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-//        primaryColor: Colors.blueAccent,
-        primarySwatch: Colors.blue
-      ),
-      initialRoute: '/',
-      routes: {
-        '/' : (context) => FlutterEffectHome(),
-        'Scaffold' : (context) => ScaffoldModule(),
-        'AppBar' : (context) => AppBarModule(),
+    return AuthProvider(
+      auth: Auth(),
+      child: MaterialApp(
+        title: 'FlutterEffect',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: RootPage(),
+        routes: {
+          'Scaffold' : (context) => ScaffoldModule(),
+          'AppBar' : (context) => AppBarModule(),
 //        'Drawer' : (context) => DrawerModule(),
-    },
-      debugShowCheckedModeBanner: false,
-//      home: FlutterEffectHome(),
+        },
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
