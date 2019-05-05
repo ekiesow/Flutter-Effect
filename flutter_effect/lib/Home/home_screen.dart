862fbaed235f8114b7e6c55132532a140296dc0a
ItsTheRadishSpirit <apope4@mail.csuchico.edu>
2019-05-04 21:03:09 -0700

    Authentication

--- flutter_effect/lib/Home/home_screen.dart
+++ flutter_effect/lib/Home/home_screen.dart
@@ -1,7 +1,19 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_effect/drawer.dart';
+import 'package:flutter_effect/Auth/auth.dart';
+import 'package:flutter_effect/Auth/auth_provider.dart';
+import 'package:flutter_effect/Home/drawer.dart';
 
 class FlutterEffectHome extends StatelessWidget {
+
+
+  Future<void> _signOut(BuildContext context) async {
+    try {
+      final BaseAuth auth = AuthProvider.of(context).auth;
+      await auth.signOut();
+    } catch (e) {
+      print(e);
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -9,6 +21,12 @@ class FlutterEffectHome extends StatelessWidget {
         backgroundColor:  Theme.of(context).backgroundColor,
         title: Text("Flutter Effect",
         ),
+        actions: <Widget>[
+          FlatButton(
+            child: Text('Logout', style: TextStyle(fontSize: 17.0, color: Colors.white)),
+            onPressed: () => _signOut(context),
+          )
+        ],
         centerTitle: true,
       ),
       drawer: MainDrawer(),
--- flutter_effect/lib/main.dart
+++ flutter_effect/lib/main.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_effect/Home/home_screen.dart';
+import 'package:flutter_effect/Auth/auth.dart';
+import 'package:flutter_effect/Auth/auth_provider.dart';
+import 'package:flutter_effect/Auth/root_page.dart';
 
 void main() => runApp(FlutterEffect());
 
 class FlutterEffect extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-//        primaryColor: Colors.blueAccent,
-        primarySwatch: Colors.blue
+    return AuthProvider(
+      auth: Auth(),
+      child: MaterialApp(
+        title: 'FlutterEffect',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: RootPage(),
       ),
-      debugShowCheckedModeBanner: false,
-      home: FlutterEffectHome(),
     );
   }
 }
