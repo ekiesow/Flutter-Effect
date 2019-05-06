e5f1ef99590a061f105a4796f5071be253a0580f
Eric Kiesow <ekiesow@mail.csuchico.edu>
2019-05-06 13:32:50 -0700

    Prep for WebView.

---
+++ flutter_effect/lib/Home/modules/drawer/module_code.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class MyDrawer extends StatefulWidget {
+  @override
+  _MyDrawerState createState() => _MyDrawerState();
+}
+
+class _MyDrawerState extends State<MyDrawer> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          DrawerHeader(
+            child: Text('Drawer Header',
+            ),
+            decoration: BoxDecoration(
+              color: Theme
+                  .of(context)
+                  .backgroundColor,
+            ),
+          ),
+          ListTile(
+              title: Text("Close"),
+              trailing: Icon(Icons.clear),
+              onTap: () {
+                Navigator.pop(context);
+              }
+          ),
+        ],
+      ),
+    );
+  }
+}
